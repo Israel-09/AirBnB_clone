1b1b9ce836f8808cac94f839547203cb421d3b5c
israel-09 <israelinene91@gmail.com>
2023-07-17 10:39:35 +0000

    uploaded test cases

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -17,3 +17,19 @@ class BaseModelTest(unittest.TestCase):
         self.assertTrue(hasattr(self.model1, 'id'))
         self.assertTrue(type(self.model1.id), str)
         self.assertNotEqual(self.model1.id, self.model2.id)
+
+    def test_base_model_created_at(self):
+        '''testing the created_at attrinbute'''
+        self.assertTrue(hasattr(self.model1, 'created_at'))
+        self.assertTrue(type(self.model1.created_at), 'datetime.datetime')
+
+    def test_base_model_updated_at(self):
+        '''testing the updated_at attrinbute'''
+        self.assertTrue(hasattr(self.model1, 'updated_at'))
+        self.assertTrue(type(self.model1.updated_at), 'datetime.datetime')
+        self.assertNotEqual(self.model1.created_at, self.model2.created_at)
+
+    def test_base_model_save(self)
+        '''testing the save method of BaseModel'''
+        self.model1.save()
+        
