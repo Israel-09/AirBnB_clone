a97a7d9f8cd2c381a7d4dedc40c37547aa85f98b
Israel-09 <israelinene91@gmail.com>
2023-07-17 10:15:05 -0700

    remove call to storage

--- console.py
+++ console.py
@@ -8,7 +8,6 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
