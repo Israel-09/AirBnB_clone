df0b69dcba50ab57951140d2be625d8447f3aea4
israel-09 <israelinene91@gmail.com>
2023-07-18 14:48:33 +0000

    more test case

--- test_model.py
+++ test_model.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python3
+from models import storage
+import os
 from models.base_model import BaseModel
 
+if os.path.exists('file.json'):
+    os.remove('file.json')
+else:
+    print('unavailable')
+'''all_objs = storage.all()
 my_model = BaseModel()
-my_model.name = "My First Model"
+print(len(storage.all()) == 1)
+'''
+'''
+print("-- Reloaded objects --")
+for obj_id in all_objs.keys():
+    obj = all_objs[obj_id]
+    print(obj)
+
+print("-- Create a new object --")
+my_model = BaseModel()
+my_model.name = "My_First_Model"
 my_model.my_number = 89
-print(my_model)
 my_model.save()
 print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+'''
